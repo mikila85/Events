ebbbe3283cf1b4d01c1320a7ae9ebcb85535a934
mikila85 <mikila85@gmail.com>
2013-10-20 20:12:22 +0200

    404

---
+++ app/views/errors/404.php
@@ -0,0 +1,9 @@
+<h1>error 404</h1>
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mike
+ * Date: 10/20/13
+ * Time: 6:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
